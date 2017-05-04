7792b4a397cfc07630b5ed6683cfe7d0792537de
Patrice De Saint Steban <patrice@desaintsteban.fr>
2017-05-04 23:57:35 +0200

    Remove thing

--- sfeirchallenge/src/pages/view-thing/view-thing.ts
+++ sfeirchallenge/src/pages/view-thing/view-thing.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams, ToastController } from 'ionic-angular';
+import { NavController, NavParams, ToastController, AlertController } from 'ionic-angular';
 import { HallService } from "../../providers/hall.service";
 import { ThingsService } from "../../providers/things.service";
 import { UserData } from "../../providers/user-data";
 import { UpdateThing } from '../../pages/update-thing/update-thing';
-import { Hall, Thing, Model} from "../../providers/hallthings";
+import { Hall, Thing, Model } from "../../providers/hallthings";
 
 @Component({
   selector: 'view-thing',
@@ -18,7 +18,7 @@ export class ViewThing {
   values = {};
   model: Model;
 
-  constructor(public nav: NavController, public navParams: NavParams, private toastCtrl: ToastController, private HallService: HallService, private ThingsService: ThingsService, private UserData: UserData) {
+  constructor(public nav: NavController, public navParams: NavParams, private toastCtrl: ToastController, private alertCtrl: AlertController, private HallService: HallService, private ThingsService: ThingsService, private UserData: UserData) {
     this.thingId = navParams.get('thingId');
     this.hallId = navParams.get('hallId');
 
@@ -34,5 +34,32 @@ export class ViewThing {
 
   update() {
     this.nav.push(UpdateThing, {hallId: this.hallId, thingId: this.thingId});
+  }
+
+  delete() {
+    this.alertCtrl.create({
+    title: 'Suppression',
+    message: 'Voulez-vous vraiment supprimer ce thing ?',
+    buttons: [
+      {
+        text: 'non',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      },
+      {
+        text: 'Oui',
+        handler: () => {
+          this.hall.properties = [];
+          this.thing = undefined;
+          this.ThingsService.delete(this.hallId, this.thingId).then(() => {
+              console.log("Success delete");
+              this.nav.pop();
+          });
+        }
+      }
+    ]
+  }).present();
   }
 }
--- sfeirchallenge/src/providers/things.service.ts
+++ sfeirchallenge/src/providers/things.service.ts
@@ -5,6 +5,7 @@ import { AngularFire, FirebaseListObservable, FirebaseObjectObservable } from 'a
 
 @Injectable()
 export class ThingsService {
+
     constructor(private af: AngularFire) {
 
     }
@@ -23,5 +24,9 @@ export class ThingsService {
 
     update(hallId:string, thing:Thing, thingKey: string):firebase.Promise<void> {
       return this.af.database.object('things/'+hallId+'/'+thingKey).update(thing);
+    }
+
+    delete(hallId: string, thingId: string): firebase.Promise<void> {
+      return this.af.database.object('things/'+hallId+'/'+thingId).remove();
     }
 }
