3a5a83f076e87e6009147fdb1d90d876ef616be2
Patrice De Saint Steban <patrice@desaintsteban.fr>
2017-05-05 00:33:59 +0200

    Protection contre les values vides

--- sfeirchallenge/functions/index.js
+++ sfeirchallenge/functions/index.js
@@ -35,36 +35,36 @@ exports.updatePropertiesHalls = functions.database.ref('/halls/{hallId}/properti
           switch (property.type) {
             case "TEXT":
               tagsIndex = tagsIndex < tags.length-1 ? tagsIndex + 1 : tags.length-1;
-              html.append(`<${tag}>{{values.${property.name}}}</${tag}>`);
+              html.append(`<${tag}>{{values?.${property.name}}}</${tag}>`);
               break;
             case "NUMBER":
-              html.append(`<div item-right>{{values.${property.name} | number}}</div item-right>`);
+              html.append(`<div item-right>{{values?.${property.name} | number}}</div item-right>`);
               break;
             case "DATE":
               tagsIndex = tagsIndex < tags.length-1 ? tagsIndex + 1 : tags.length-1;
-              html.append(`<${tag}>{{values.${property.name} | date:"dd/MM/yyyy"}}</${tag}>`);
+              html.append(`<${tag}>{{values?.${property.name} | date:"dd/MM/yyyy"}}</${tag}>`);
               break;
             case "DATETIME":
               tagsIndex = tagsIndex < tags.length-1 ? tagsIndex + 1 : tags.length-1;
-              html.append(`<${tag}>{{values.${property.name} | date:"dd/MM/yyyy HH:mm"}}</${tag}>`);
+              html.append(`<${tag}>{{values?.${property.name} | date:"dd/MM/yyyy HH:mm"}}</${tag}>`);
               break;
             case "YEAR":
-              html.append(`<div item-right>{{values.${property.name} | date:"yyyy"}}</div>`);
+              html.append(`<div item-right>{{values?.${property.name} | date:"yyyy"}}</div>`);
               break;
             case "TEXTAREA":
-              html.append(`<p>{{values.${property.name}}}</p>`);
+              html.append(`<p>{{values?.${property.name}}}</p>`);
               break;
             case "ICON":
-              html.insert(`<ion-icon [name]="values.${property.name}" item-left></ion-icon>`, 0);
+              html.insert(`<ion-icon [name]="values?.${property.name}" item-left></ion-icon>`, 0);
               break;
             case "COLOR":
-              attr += ` [style.backgroundColor]="values.${property.name}"`;
+              attr += ` [style.backgroundColor]="values?.${property.name}"`;
               break;
             case "PICTURE":
-              html.insert(`<picture-thumbnail [image]=\"values.${property.name}\"  item-left></picture-thumbnail>`, 0);
+              html.insert(`<picture-thumbnail [image]=\"values?.${property.name}\"  item-left></picture-thumbnail>`, 0);
               break;
             case "STAR":
-              html.append(`<div item-right><ion-icon *ngFor="let rate of [1,2,3,4,5]" [name]="rate <= values.${property.name} ? 'star' : 'star-outline'"></ion-icon></div>`)
+              html.append(`<div item-right><ion-icon *ngFor="let rate of [1,2,3,4,5]" [name]="rate <= values?.${property.name} ? 'star' : 'star-outline'"></ion-icon></div>`)
               break;
           }
         }
